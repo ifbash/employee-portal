94870ce3cda92d86375272863f09aa14fc395c11
ifBash <ifbashapp@gmail.com>
2025-11-21 19:13:26 +0530

    Add CalendarPage component with Calendar integration

---
+++ app/calendar/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import Calendar from '@/components/calendar/Calendar';
+
+export default function CalendarPage() {
+  return (
+    <div className="p-6">
+      <Calendar />
+    </div>
+  );
+}
